4faf8a8c9e8dbed162be0adb02d0cfc0876dc18c
BaptisteArati <baptiste.arati@gmail.com>
2017-12-16 19:00:57 +0100

    Interface Update -Added Check Boxes for selecting user data -Incremental row value for software flexibility

--- PyCharmProject/classes/interface.py
+++ PyCharmProject/classes/interface.py
@@ -21,9 +21,88 @@ class Interface(Frame):
         fileMenu.add_command(label="Open", command=self.onOpen)
         menubar.add_cascade(label="File", menu=fileMenu)
 
-        self.txt = Text(self)
-        self.txt.pack(fill=BOTH, expand=1)
+        rowNumber = 0
 
+        Label(self, text="Standard").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isMaxLoad = IntVar()
+        Checkbutton(self, text="Max Load", variable=isMaxLoad).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isLoadingRate = IntVar()
+        Checkbutton(self, text="Loading Rate", variable=isLoadingRate).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isPause = IntVar()
+        Checkbutton(self, text="Pause", variable=isPause).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="UNHT S/N").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isFnContact = IntVar()
+        Checkbutton(self, text="Fn Contact", variable=isFnContact).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isApproachSpeed = IntVar()
+        Checkbutton(self, text="Approach Speed", variable=isApproachSpeed).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isX = IntVar()
+        Checkbutton(self, text="X Position", variable=isX).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isY = IntVar()
+        Checkbutton(self, text="Y Position", variable=isY).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isZ = IntVar()
+        Checkbutton(self, text="Z Position", variable=isZ).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="Main Results").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isHit = IntVar()
+        Checkbutton(self, text="Hit", variable=isHit).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isEit = IntVar()
+        Checkbutton(self, text="Eit", variable=isEit).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isE = IntVar()
+        Checkbutton(self, text="E*", variable=isE).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isPoissonRatio = IntVar()
+        Checkbutton(self, text="Poisson's Ratio", variable=isPoissonRatio).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="Additional Results").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isHvit = IntVar()
+        Checkbutton(self, text="Hvit", variable=isHvit).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isCIT = IntVar()
+        Checkbutton(self, text="Cit", variable=isCIT).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isWTot = IntVar()
+        Checkbutton(self, text="Wtot", variable=isWTot).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isWelast = IntVar()
+        Checkbutton(self, text="Welast", variable=isWelast).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isWplast = IntVar()
+        Checkbutton(self, text="Wplast", variable=isWplast).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+
+        Label(self, text="Measured Value").grid(row=rowNumber, sticky=W)
+        rowNumber += 1
+        isRaw = IntVar()
+        Checkbutton(self, text="Raw Data", variable=isRaw).grid(row=rowNumber, sticky=W)
+        rowNumber += 1
 
     def onOpen(self):
 
--- PyCharmProject/main.py
+++ PyCharmProject/main.py
@@ -5,7 +5,7 @@ def main():
 
     root = Tk()
     ex = Interface(root)
-    root.geometry("300x250+300+300")
+    root.geometry("250x650+300+300")
     root.mainloop()
 
 
